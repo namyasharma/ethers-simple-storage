61b1f9bf2a17dd932d1a383d37d0d89d4a46524e
namya <namyas@gmail.com>
2023-04-24 12:08:28 +0530

    added encryptKey.js

--- deploy.js
+++ deploy.js
@@ -1,12 +1,10 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
+require("dotenv").config();
 
 async function main() {
-  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
-  const wallet = new ethers.Wallet(
-    "0x5b3208286264f409e1873e3709d3138acf47f6cc733e74a6b47a040b50472fd8",
-    provider
-  );
+  const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
---
+++ encryptKey.js
@@ -0,0 +1,18 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonKey = await ethers.encryptKeystoreJson(
+    process.env.PRIVATE_KEY_PASSWORD,
+    process.env.PRIVATE_KEY
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
